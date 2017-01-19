a71cda1680b6eb524b1246327924716c6cda384b
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-01-18 23:30:41 -0700

    home - our non-negotiables. fix css/html structure, padding for sections and containers

--- inc/home-loop.php
+++ inc/home-loop.php
@@ -2,6 +2,8 @@
 get_template_part('inc/home-3-header-columns');
 get_template_part('inc/home-second-header');
 get_template_part('inc/home-meaningful-places');
+#get_template_part('inc/slider-what-we-do');
+get_template_part('inc/home-our-non-negotiables');
 
 
 if ( have_posts() ) : while ( have_posts() ) : the_post();
---
+++ inc/home-our-non-negotiables.php
@@ -0,0 +1,10 @@
+<section class="container-fluid non-negotiables white-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-12">
+                <h2>Our Non-Negotiables</h2>
+                <?php the_field('our_non_negotiables_description'); ?>
+            </div>
+        </div>
+    </div>
+</section>
