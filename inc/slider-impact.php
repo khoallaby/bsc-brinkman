e069012d7ac1ddcd63e4d0e75984cd4515313985
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-01-19 15:16:11 -0700

    acf - impact slider

--- inc/slider-impact.php
+++ inc/slider-impact.php
@@ -4,7 +4,10 @@
             <div class="col-xs-12 col-md-12">
                 <h2>Impact</h2>
                 <hr class="blue-bar" />
-			    <?php the_field('impact_slider'); ?>
+			    <?php
+                if( get_field('impact_slider') )
+                    echo do_shortcode( get_field('impact_slider') );
+                ?>
             </div>
         </div>
     </div>
