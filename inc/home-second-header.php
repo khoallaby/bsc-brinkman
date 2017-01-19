391f08b2db8d33aedd07dfb366d1341bbbcae29f
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-01-18 22:08:01 -0700

    second header styling and markup

---
+++ inc/home-second-header.php
@@ -0,0 +1,12 @@
+<section class="container-fluid second-header white-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-5">
+                <h2><?php the_field('second_header_title'); ?></h2>
+            </div>
+            <div class="col-xs-12 col-md-7">
+                <?php the_field('second_header_description'); ?>
+            </div>
+        </div>
+    </div>
+</section>
