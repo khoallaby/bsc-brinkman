5ec0e2c6dd6c3ba9e28f965f547229f69026f766
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-01-28 22:25:21 -0700

    fix single project, missing section containers

--- inc/single-loop.php
+++ inc/single-loop.php
@@ -1,50 +1,44 @@
-<div class="col-xs-12 col-sm-8 col-md-8">
-	<?php
-    if ( have_posts() ) : while ( have_posts() ) : the_post();
-	    if(has_post_thumbnail()) {
-	        the_post_thumbnail('large', array('itemprop' => 'image','class' => 'img-responsive'));
-	    }
-    ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <?php
+<section class="container-fluid content white-bg">
+    <div class="container">
+        <div class="row no-gutter">
 
-        get_template_part('inc/page-header');
-
-
-        the_content();
-
-        if( have_rows('after_content') ):
-            echo '<ul class="content-list list-unstyled">';
-            while ( have_rows('after_content') ) : the_row();
-                $image = get_sub_field('image');
-                ?>
-                <li class="row">
-                    <div class="col-xs-12 col-md-1 image">
-                        <img src="<?php echo $image['url']; ?>" /><br />
-                    </div>
-                    <div class="col-xs-12 col-md-11 contents">
-                        <h2><?php echo get_sub_field('title'); ?></h2>
-                        <?php echo get_sub_field('description'); ?>
-                    </div>
-                </li>
+            <div class="col-xs-12 col-sm-8 col-md-8">
                 <?php
-            endwhile;
-	        echo '</ul>';
-        endif;
-        ?>
+                if ( have_posts() ) : while ( have_posts() ) : the_post();
+                    if(has_post_thumbnail()) {
+                        the_post_thumbnail('large', array('itemprop' => 'image','class' => 'img-responsive'));
+                    }
+                ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <?php
+                    get_template_part('inc/page-header');
+                    the_content();
+                    if( have_rows('after_content') ):
+                        echo '<ul class="content-list list-unstyled">';
+                        while ( have_rows('after_content') ) : the_row();
+                            $image = get_sub_field('image');
+                            ?>
+                            <li class="row">
+                                <div class="col-xs-12 col-md-1 image">
+                                    <img src="<?php echo $image['url']; ?>" /><br />
+                                </div>
+                                <div class="col-xs-12 col-md-11 contents">
+                                    <h2><?php echo get_sub_field('title'); ?></h2>
+                                    <?php echo get_sub_field('description'); ?>
+                                </div>
+                            </li>
+                            <?php
+                        endwhile;
+                        echo '</ul>';
+                    endif;
+                    ?>
 
-    </article>
-    <div class="comments">
-        <?php 
-        
-            // Include the single post content template. 
-            get_template_part( 'comments'); 
-            
-            // If comments are open or we have at least one comment, load up the comment template. 
-            if 
-                ( comments_open() || get_comments_number() ) { comments_template(); 
-            }
-        ?>
+                </article>
+                <?php endwhile; endif; ?>
+            </div>
+
+            <?php get_sidebar(); ?>
+
+        </div>
     </div>
-    <?php endwhile; endif; ?>
-</div>
+</section>
--- single-projects.php
+++ single-projects.php
@@ -1,7 +1,6 @@
 <?php 
 	get_header();
 	get_template_part('inc/single-loop');
-	get_sidebar();
 	get_template_part('inc/slider-what-we-do');
 	get_footer(); 
 ?>
