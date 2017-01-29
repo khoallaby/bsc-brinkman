d4eff32a08303625b3560592620eebbb1213158d
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-01-28 22:29:59 -0700

    non negotiable page

---
+++ page-non-negotiables.php
@@ -0,0 +1,30 @@
+<?php
+/* Template Name: Non Negotiables */
+get_header();
+get_template_part('inc/page-loop');
+
+
+if( have_rows('banners') ):
+	while ( have_rows('banners') ) : the_row();
+		$image = get_sub_field('image');
+		?>
+		<section class="container-fluid banner-nn" style="background-image: url('<?php echo $image['url']; ?>');">
+			<div class="container">
+				<div class="row no-gutter">
+					<div class="col-xs-12 col-md-12">
+						<div class="col-xs-12 col-md-5">
+							<h2><?php the_sub_field('title'); ?></h2>
+						</div>
+						<div class="col-xs-12 col-md-7">
+							<?php the_sub_field('description'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<?php
+	endwhile;
+endif;
+
+
+get_footer();
