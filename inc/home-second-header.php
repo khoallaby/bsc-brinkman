982e5b465920303be09e913c4b0a61408731c29c
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-01-28 22:32:13 -0700

    rounded header

--- inc/home-second-header.php
+++ inc/home-second-header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-5">
-                <h2><?php the_field('second_header_title'); ?></h2>
+                <h2 class="rounded-header"><?php the_field('second_header_title'); ?></h2>
             </div>
             <div class="col-xs-12 col-md-7">
                 <?php the_field('second_header_description'); ?>
