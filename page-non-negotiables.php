caaf8bea2bb0a0f62eea9fa2c3af82d200f7fe8b
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-01-28 22:56:41 -0700

    finalize non negotiables page

--- page-non-negotiables.php
+++ page-non-negotiables.php
@@ -9,13 +9,13 @@ if( have_rows('banners') ):
 		$image = get_sub_field('image');
 		?>
 		<section class="container-fluid banner-nn" style="background-image: url('<?php echo $image['url']; ?>');">
-			<div class="container">
+			<div class="container vcenter">
 				<div class="row no-gutter">
-					<div class="col-xs-12 col-md-12">
-						<div class="col-xs-12 col-md-5">
-							<h2><?php the_sub_field('title'); ?></h2>
-						</div>
+					<div class="col-md-12">
 						<div class="col-xs-12 col-md-7">
+							<h2 class="rounded-header"><?php the_sub_field('title'); ?></h2>
+						</div>
+						<div class="col-xs-12 col-md-5">
 							<?php the_sub_field('description'); ?>
 						</div>
 					</div>
