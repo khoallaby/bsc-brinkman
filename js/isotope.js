2c2d4452aab7b6b58021c5a230cdaf88e111924f
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-02-12 20:38:27 -0700

    add url filtering for isotope. fixed weird image positioning when reisizing the window.

--- js/isotope.js
+++ js/isotope.js
@@ -1,54 +1,71 @@
 (function($){
     $(document).ready(function() {
 
-        // init Isotope
-        var $grid = $('.grid').isotope({
-            layoutMode: 'masonry',
-            itemSelector: '.grid-item',
-            percentPosition: true,
-            //stamp: '.stamp',
-            //stamp: $grid.find('.tall'),
-
-            getSortData: {
-                name: 'h3 a',
-                order: '[data-order] parseInt',
-                category: '[data-category]'
-            },
-            sortBy : 'order', //'original-order',
-            sortAscending: true,
-            masonry: {
-                columnWidth: '.col-md-4',
-                isFitWidth: true
-            }
-        });
 
 
-        /*
-        $(window).smartresize(function(){
-            $grid.isotope({
-                columnWidth: '.col-md-4'
-            });
-            $grid('layout');
-        });
+        function getHashFilter() {
+            // get filter=filterName
+            var matches = location.hash.match( /filter=([^&]+)/i );
+            var hashFilter = matches && matches[1];
+            return hashFilter && decodeURIComponent( hashFilter );
+        }
 
-        $(window).resize(function () {
-            $grid('layout');
-        });
-        */
 
         // bind filter button click
-        $('.filters-button-group').on( 'click', 'button', function() {
-            var filterValue = $( this ).attr('data-filter');
+        var $filterButtonGroup = $('.button-group');
+        $filterButtonGroup.on( 'click', 'button', function() {
+            var filterAttr = $( this ).attr('data-filter');
+            // set filter in hash
+            location.hash = 'filter=' + encodeURIComponent( filterAttr );
+            // and filter stuff
             $grid.isotope({ filter: filterValue });
         });
-        // change is-checked class on buttons
-        $('.button-group').each( function( i, buttonGroup ) {
-            var $buttonGroup = $( buttonGroup );
-            $buttonGroup.on( 'click', 'button', function() {
-                $buttonGroup.find('.active').removeClass('active');
-                $( this ).addClass('active');
+
+
+        // init Isotope
+        var $grid = $('.grid');
+        var isIsotopeInit = false;
+
+
+        function onHashchange() {
+            var hashFilter = getHashFilter();
+            if ( !hashFilter && isIsotopeInit ) {
+                return;
+            }
+            isIsotopeInit = true;
+            // filter isotope
+            $grid.isotope({
+                layoutMode: 'masonry',
+                itemSelector: '.grid-item',
+                percentPosition: true,
+                //stamp: '.stamp',
+                //stamp: $grid.find('.tall'),
+
+                filter: hashFilter,
+
+                getSortData: {
+                    name: 'h3 a',
+                    order: '[data-order] parseInt',
+                    category: '[data-category]'
+                },
+                sortBy : 'order', //'original-order',
+                sortAscending: true,
+                masonry: {
+                    columnWidth: '.col-md-4'
+                }
             });
-        });
+            // set selected class on button
+            if ( hashFilter ) {
+                $filterButtonGroup.find('.active').removeClass('active');
+                $filterButtonGroup.find('[data-filter="' + hashFilter + '"]').addClass('active');
+            }
+        }
+
+        $(window).on( 'hashchange', onHashchange );
+
+        // trigger event handler to init Isotope
+        onHashchange();
+
 
 
     });
