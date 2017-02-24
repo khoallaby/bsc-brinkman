f1b549451eb0a2039595fa3d3f6a458cf134f810
Andy Nguyen <andrew.nguyen@colorado.edu>
2017-02-24 14:06:28 -0600

    fix sidebar on team members page

--- inc/single-loop.php
+++ inc/single-loop.php
@@ -31,7 +31,7 @@
                 ?>
         </div>
         <div class="row no-gutter">
-    <?php $col_size = is_singular('projects') ? 'col-sm-8 col-md-8' : 'col-sm-9 col-md-9'; ?>
+    <?php $col_size = is_singular('projects') || is_singular('team-members') ? 'col-sm-8 col-md-8' : 'col-sm-9 col-md-9'; ?>
             <div class="col-xs-12 <?php echo $col_size; ?>">
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <?php
